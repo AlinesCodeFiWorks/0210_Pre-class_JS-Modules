5c6ed0a386360f089d2d62ccd3026994e15d22ae
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-02-09 21:43:17 -0600

    Completed pre-class exercise!

---
+++ mathUtils.js
@@ -0,0 +1,9 @@
+//Exercise
+//Write two functions: one for adding two numbers (add) and another for multiplying two numbers (multiply).
+export function add(a, b) {
+  return a + b;
+}
+
+export function multiply(a, b) {
+  return a * b;
+}
